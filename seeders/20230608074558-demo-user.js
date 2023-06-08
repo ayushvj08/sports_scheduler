85f8e83aac2153205a61b94a9c6e47a33fbac82a
Ayush Vijayant <ayush.vijayant@gmail.com>
2023-06-08 13:29:59 +0530

    add await

--- seeders/20230608074558-demo-user.js
+++ seeders/20230608074558-demo-user.js
@@ -14,7 +14,7 @@ module.exports = {
      * }], {});
      */
 
-    return queryInterface.bulkInsert("User", [
+    return await queryInterface.bulkInsert("User", [
       {
         firstName: "John",
         lastName: "Doe",
